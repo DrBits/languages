fa03695081e0d0cf33d2a6da3813e38b57707741
Alex Levanov <ilewanof@gmail.com>
2022-12-26 10:01:27 +0100

    Update docker for connect backend and frontend; add migrations

--- client/src/core/stores/users.store.js
+++ client/src/core/stores/users.store.js
@@ -1,5 +1,10 @@
+import axios from 'axios';
 import create from 'zustand';
 
-export const useUsersStore = create(() => ({
-  users: []
+export const useUsersStore = create((set) => ({
+  users: [],
+  signup: async (user) => {
+    const response = axios.post('api/users', { user });
+    set({ users: response.data });
+  }
 }));
--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -1,9 +1,21 @@
-import React from 'react';
-import { Text, TextBox } from '@app/core/ui-library';
+import React, { useState } from 'react';
+import { useUsersStore } from '@app/core/stores/users.store';
+import { Text, TextBox, Button } from '@app/core/ui-library';
 
 import { Container, BaseCenter } from './styles';
 
 const Home = () => {
+  const [user, setUser] = useState({
+    username: '',
+    email: '',
+    password: ''
+  });
+  const signup = useUsersStore((state) => state.signup);
+
+  const onClickSignUp = () => {
+    signup(user);
+  };
+
   return (
     <Container>
       <BaseCenter>
@@ -14,7 +26,36 @@ const Home = () => {
           Whereas a common understanding of these rights and freedoms is
         </Text>
         <div style={{ marginTop: '2rem' }}>
-          <TextBox label="label" size="medium" />
+          <TextBox
+            label="username"
+            size="medium"
+            onChange={(value) =>
+              setUser((prevState) => ({ ...prevState, username: value }))
+            }
+          />
+        </div>
+        <div style={{ marginTop: '2rem' }}>
+          <TextBox
+            label="Email"
+            size="medium"
+            onChange={(value) =>
+              setUser((prevState) => ({ ...prevState, email: value }))
+            }
+          />
+        </div>
+        <div style={{ marginTop: '2rem' }}>
+          <TextBox
+            label="Password"
+            size="medium"
+            onChange={(value) =>
+              setUser((prevState) => ({ ...prevState, password: value }))
+            }
+          />
+        </div>
+        <div style={{ marginTop: '2rem' }}>
+          <Button size="medium" onClick={onClickSignUp}>
+            SignUp
+          </Button>
         </div>
       </BaseCenter>
     </Container>
